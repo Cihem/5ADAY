f6ec0d2a271ccc902b636c164b77adb37cdd0aee
Cihem <cihemzine@gmail.com>
2020-05-26 21:43:09 +0200

    saving read me file

--- client/src/ApiRecipe.js
+++ client/src/ApiRecipe.js
@@ -19,7 +19,7 @@ useEffect(  () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
+    // console.log(data.hits);
     
  }
     
